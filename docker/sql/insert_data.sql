53bf16b129c2341075f02d65d46e72de352d4b6c
nakasone <nakasone@std.it-college.ac.jp>
2022-06-24 10:20:58 +0900

    mariadb の構成変更
    初期データを自動で取り込むよう変更
    文字コードを設定ファイルで指定

--- docker/sql/insert_data.sql
+++ docker/sql/insert_data.sql
@@ -1,2 +1,6 @@
 INSERT INTO category (name)
-VALUES ('トピックス'), ('IR'),('採用'),('その他')
+VALUES ('トピックス'), ('IR'),('採用'),('その他');
+
+INSERT INTO users (username, password, view_name, role_type)
+VALUES ('admin', '$2y$05$JV3MtWH9zM1z0I1.ytUmS.5UjiIXEHU0k4uwI3.MFESqIhKEUmcim', '最初の管理者', 'ADMIN'),
+('user','$2y$05$1TGomTpOpQwkvTp/OOApfOUymKx/BNlc7CdA9MvCiuz4rNntC4e5C','ゆーざ', 'USER')
