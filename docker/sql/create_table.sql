7cd81be899a3a39bced93e04321b5b84c14d74c3
nakasone <nakasone@std.it-college.ac.jp>
2022-06-30 10:31:42 +0900

    色々調整しました

--- docker/sql/create_table.sql
+++ docker/sql/create_table.sql
@@ -1,22 +1,22 @@
 CREATE TABLE users (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(256) NOT NULL UNIQUE ,
-    password VARCHAR(256) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(256) NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL,
     view_name VARCHAR(256) NOT NULL,
     role_type ENUM('ADMIN','USER') NOT NULL
 );
 
-CREATE TABLE category (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(128) NOT NULL
 );
 
-CREATE TABLE news(
+CREATE TABLE news (
     id    BIGINT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(256) NOT NULL,
-    body  LONGTEXT,
-    category_id BIGINT NOT NULL,
-    publish_at DATETIME,
-    create_at DATETIME DEFAULT NOW(),
-    user_id BIGINT NOT NULL
+    body  TEXT,
+    published_at DATETIME,
+    created_at DATETIME DEFAULT NOW(),
+    category_id INT NOT NULL REFERENCES categories(id),
+    user_id INT NOT NULL REFERENCES users(id)
 );
